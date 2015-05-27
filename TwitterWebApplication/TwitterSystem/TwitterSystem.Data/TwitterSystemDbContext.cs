fe874e63bb17666e62079bb998c52d83d549f935
georgilambov <g_lambov@bitex.bg>
2015-05-27 18:28:04 +0300

    add docks and structure

---
+++ TwitterWebApplication/TwitterSystem/TwitterSystem.Data/TwitterSystemDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace TwitterSystem.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+
+    public class TwitterSystemDbContext : IdentityDbContext<User>
+    {
+        public TwitterSystemDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static TwitterSystemDbContext Create()
+        {
+            return new TwitterSystemDbContext();
+        }
+    }
+}
